c580a73e836102374a9027feb21f8c76bb91adb7
Draugr deathlord Horker <dhorker@oa-k3.21-school.ru>
2019-11-10 14:57:44 +0300

    ContextMenu1

---
+++ Assets/Scripts/ContextMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContextMenu : MonoBehaviour
+{
+    private Color basicColor;
+    private Color hoverColor = new Color(0.1f, 0.1f, 0.1f, 1);
+    private Renderer _renderer;
+
+    [SerializeField] private GameObject targetCanvas;
+
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        basicColor = _renderer.material.color;
+    }
+
+    void OnMouseEnter()
+    {
+        _renderer.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        _renderer.material.color = basicColor;
+    }
+
+    private void OnMouseDown()
+    {
+
+    }
+}
